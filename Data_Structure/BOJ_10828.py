735176bb0c6af001cee59534915a96c814cdffe3
HoYoungChun <ho723@naver.com>
2022-04-27 00:40:28 +0900

    백준 10828번

---
+++ Data_Structure/BOJ_10828.py
@@ -0,0 +1,21 @@
+
+if __name__ == "__main__":
+    import sys
+    input = sys.stdin.readline  # 시간초과 막기 위해
+    N = int(input())
+    stack = []
+
+    for _ in range(N):
+        command = input().split()
+        if command[0] == "push":
+            stack.append(command[1])
+        elif command[0] == "pop":
+            print(stack.pop()) if stack else print(-1)
+        elif command[0] == "size":
+            print(len(stack))
+        elif command[0] == "empty":
+            print(1) if not stack else print(0)
+        elif command[0] == "top":
+            print(stack[-1]) if stack else print(-1)
+        else:  # 문제조건으로 여기 올 일 없다
+            pass
